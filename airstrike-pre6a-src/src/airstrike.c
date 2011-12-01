79e8bede834520830996b78e2b4d8448105a00e4
sauvage <noki.sauvage@gmail.com>
2011-12-01 11:26:16 +0100

    modif Makefile pour que ca compile chez moi, et corrigé main airstrike récupération des arguments

--- airstrike-pre6a-src/src/airstrike.c
+++ airstrike-pre6a-src/src/airstrike.c
@@ -33,11 +33,10 @@ static int screen_h;
 static int nbTeams;
 static int nbPlayers;
 
-
 static void atexit_cleanup(void)
 {
 	/* Since we don't want a crash in fullscreen mode
-     we hope that this will bring us back to a window */
+	 we hope that this will bring us back to a window */
 	SDL_SetVideoMode(screen_w, screen_h, 0, 0);
 }
 
@@ -48,37 +47,37 @@ static int general_setup(void)
 	maths_setup();
 	config_setup();
 
-	screen_w = cfgnum("screen.width",800);
-	screen_h = cfgnum("screen.height",600);
-	if (cfgnum("fullscreen",0) == 1)
+	screen_w = cfgnum("screen.width", 800);
+	screen_h = cfgnum("screen.height", 600);
+	if (cfgnum("fullscreen", 0) == 1)
 		displayflags |= SDL_FULLSCREEN;
 
-	sprite_global.opt_alpha = cfgnum("alpha",0);
+	sprite_global.opt_alpha = cfgnum("alpha", 0);
 
 	i = SDL_Init(SDL_INITFLAGS);
 	assert(i == 0);
 
 	atexit(atexit_cleanup);
 
-	SDL_WM_SetCaption(VERSIONSTRING,0);
+	SDL_WM_SetCaption(VERSIONSTRING, 0);
 
 	sprite_global.display = SDL_SetVideoMode(screen_w, screen_h, 0, displayflags);
 	assert(sprite_global.display);
 	SDL_ShowCursor(SDL_DISABLE);
 
-	if (cfgnum("sound",0) == 1)
+	if (cfgnum("sound", 0) == 1)
 	{
 		sound_setup();
 		/* place the ear on the ground, in the center */
-		sound_move_ear(screen_w/2,screen_h);
+		sound_move_ear(screen_w / 2, screen_h);
 	}
 	sprite_setup();
 	text_setup();
 	console_setup();
-	console_set_pos(9,254);
+	console_set_pos(9, 254);
 	console_load_bg(path_to_data("console-bg.png"));
 	sprite_types_setup();
-	sprite_background_load("data/bg.png","data/bgmask.png");
+	sprite_background_load("data/bg.png", "data/bgmask.png");
 
 	level_setup();
 	winds_setup();
@@ -89,21 +88,23 @@ static int general_setup(void)
 void players_setup(void)
 {
 	sprite_t *sp;
-	playerCount =cfgnum("nr_players",nbPlayers);
+	//playerCount = cfgnum("nr_players", nbPlayers);
+	// Number of players is set up at launch as an argument
+	playerCount = nbPlayers;
 	player_init();
 	assert((playerCount >= 0) && (playerCount <= MAXPLAYERS));
 	int i;
-	for(i=0;i< playerCount;i++){
+	for (i = 0; i < playerCount; i++)
+	{
 		players[i].sprite_type = &blueplane;
 		players[i].sprite_type = &blueplane;
 		//	players[i].sprite_type = &biplane;
-		players[i].startpos[0] = 100*i;
-		players[i].startpos[1] = 300+20*(i%10);
+		players[i].startpos[0] = 100 * i;
+		players[i].startpos[1] = 300 + 20 * (i % 10);
 		players[i].points = max_points;
 		player_sethuman(i);
 		//player_setai(i);
 	}
-
 }
 
 void engine_setup(void)
@@ -117,54 +118,54 @@ void engine_setup(void)
 
 void objects_setup(void)
 {
-	int i,x,y;
+	int i, x, y;
 	sprite_t *sp;
 	float v[2];
 
 	srand(23);
 
 	/*sp = sprite_create(&bonusmachine);
-	sprite_group_insert(mech_group,sp);
-	sprite_set_pos(sp,level.xmax/2,15);*/
+	 sprite_group_insert(mech_group,sp);
+	 sprite_set_pos(sp,level.xmax/2,15);*/
 
 	/*for (i=0;i<3;i++)
-	{
-		sp = sprite_create(&bird);
-		sprite_group_insert(mech_group,sp);
-	}*/
+	 {
+	 sp = sprite_create(&bird);
+	 sprite_group_insert(mech_group,sp);
+	 }*/
 
 	/*for (i=0;i<5;i++)
-	{
-		sp = sprite_create(&bouncer);
-		sprite_group_insert(mech_group,sp);
-		sprite_set_pos(sp,rand() % 700 + 50,rand() % 200 + 100);
-	}*/
+	 {
+	 sp = sprite_create(&bouncer);
+	 sprite_group_insert(mech_group,sp);
+	 sprite_set_pos(sp,rand() % 700 + 50,rand() % 200 + 100);
+	 }*/
 
 	/*sp = sprite_create(&cloud);
-	sprite_group_insert(foreground_group,sp);
-	sprite_set_pos(sp,400,70);
-	v[0] = 15; v[1] = 0;
-	sprite_set_vel(sp,v);*/
+	 sprite_group_insert(foreground_group,sp);
+	 sprite_set_pos(sp,400,70);
+	 v[0] = 15; v[1] = 0;
+	 sprite_set_vel(sp,v);*/
 
 	/*sp = sprite_create(&cloud);
-	sprite_group_insert(foreground_group,sp);
-	sprite_set_pos(sp,0,150);
-	v[0] = 10; v[1] = 0;
-	sprite_set_vel(sp,v);*/
+	 sprite_group_insert(foreground_group,sp);
+	 sprite_set_pos(sp,0,150);
+	 v[0] = 10; v[1] = 0;
+	 sprite_set_vel(sp,v);*/
 
 	/*sp = sprite_create(&zeppelin);
-	sprite_group_insert(mech_group,sp);
-	sprite_set_pos(sp,50,200);*/
+	 sprite_group_insert(mech_group,sp);
+	 sprite_set_pos(sp,50,200);*/
 
 	/*sp = sprite_create(&airballoon);
-	sprite_group_insert(mech_group,sp);
-	sprite_set_pos(sp,100,150);
-	v[0] = 5; v[1] = 3;
-	sprite_set_vel(sp,v);*/
+	 sprite_group_insert(mech_group,sp);
+	 sprite_set_pos(sp,100,150);
+	 v[0] = 5; v[1] = 3;
+	 sprite_set_vel(sp,v);*/
 
 	/*sp = sprite_create(&cannon);
-	sprite_group_insert(mech_group,sp);
-	sprite_set_pos(sp,380,518);*/
+	 sprite_group_insert(mech_group,sp);
+	 sprite_set_pos(sp,380,518);*/
 }
 
 static int process_events(void)
@@ -173,17 +174,8 @@ static int process_events(void)
 	SDL_Event event;
 
 	int ret = 1;
-	while ( SDL_PollEvent(&event) >= 1 )
+	while (SDL_PollEvent(&event) >= 1)
 	{
-		if( &event == NULL )
-	{
-		printf("Pointer is NULL\n");
-		return 0;
-	}else{
-		printf("Pointer is %d\n",&event);
-	}
-		printf("Event type: %d\n",event.type);
-		printf("debug\n");
 		switch (event.type)
 		{
 		case SDL_QUIT:
@@ -202,11 +194,10 @@ static int process_events(void)
 				break;
 			}
 			break;
-			default:
-				break;
+		default:
+			break;
 		}
 	}
-	printf("out\n");
 	return ret;
 }
 
@@ -215,7 +206,7 @@ static int wait_for_key(void)
 	SDL_Event event;
 	while (1)
 	{
-		while ( SDL_PollEvent(&event) >= 1 )
+		while (SDL_PollEvent(&event) >= 1)
 		{
 			switch (event.type)
 			{
@@ -236,54 +227,38 @@ static int wait_for_key(void)
 void draw_ui(void)
 {
 	char cbuf[256];
-	float av_dt,s_dt;
+	float av_dt, s_dt;
 	int i;
 	/* left player stats */
-	text_render(sprite_global.display,0,big_font,
-			54,sprite_global.display->h - 27,
-			ALIGN_LEFT,ALIGN_BOTTOM,
-			players[1].name);
+	text_render(sprite_global.display, 0, big_font, 54, sprite_global.display->h - 27, ALIGN_LEFT, ALIGN_BOTTOM, players[1].name);
 	if (sprite_isvalid(&(players[1].sprite)))
 	{
-		sprite_signal(players[1].sprite,SIGNAL_STATSTRING,cbuf);
-		text_render(sprite_global.display,0,small_font,
-				54,sprite_global.display->h - 17,
-				ALIGN_LEFT,ALIGN_BOTTOM,cbuf);
+		sprite_signal(players[1].sprite, SIGNAL_STATSTRING, cbuf);
+		text_render(sprite_global.display, 0, small_font, 54, sprite_global.display->h - 17, ALIGN_LEFT, ALIGN_BOTTOM, cbuf);
 	}
 	/* right player stats */
-	text_render(sprite_global.display,0,big_font,
-			sprite_global.display->w - 54,
-			sprite_global.display->h - 27,
-			ALIGN_RIGHT,ALIGN_BOTTOM,players[0].name);
+	text_render(sprite_global.display, 0, big_font, sprite_global.display->w - 54, sprite_global.display->h - 27, ALIGN_RIGHT, ALIGN_BOTTOM, players[0].name);
 	if (sprite_isvalid(&(players[0].sprite)))
 	{
-		sprite_signal(players[0].sprite,SIGNAL_STATSTRING,cbuf);
-		text_render(sprite_global.display,0,small_font,
-				sprite_global.display->w - 54,
-				sprite_global.display->h - 17,
-				ALIGN_RIGHT,ALIGN_BOTTOM,cbuf);
+		sprite_signal(players[0].sprite, SIGNAL_STATSTRING, cbuf);
+		text_render(sprite_global.display, 0, small_font, sprite_global.display->w - 54, sprite_global.display->h - 17, ALIGN_RIGHT, ALIGN_BOTTOM, cbuf);
 	}
 	/* score */
-	sprintf(cbuf,"%2.2i-%2.2i\nSCORE",max_points - 1 - players[0].points,
-			max_points - 1 - players[1].points);
-	text_render(sprite_global.display,0,big_font,
-			sprite_global.display->w/2,
-			sprite_global.display->h - 5,
-			ALIGN_CENTER,ALIGN_BOTTOM,cbuf);
+	sprintf(cbuf, "%2.2i-%2.2i\nSCORE", max_points - 1 - players[0].points, max_points - 1 - players[1].points);
+	text_render(sprite_global.display, 0, big_font, sprite_global.display->w / 2, sprite_global.display->h - 5, ALIGN_CENTER, ALIGN_BOTTOM, cbuf);
 	if (show_debug)
 	{
 		/* calculate frame time stats */
 		av_dt = 0;
 		s_dt = 0;
-		for (i=0;i<32;i++)
+		for (i = 0; i < 32; i++)
 			av_dt += frame_times[i];
 		av_dt /= 32;
-		for (i=0;i<32;i++)
-			s_dt += (frame_times[i] - av_dt)*(frame_times[i] - av_dt);
-		s_dt = sqrt(s_dt/32);
-		sprintf(cbuf,"average dt = %3.2f (stddev = %3.2f)",av_dt,s_dt);
-		text_render(sprite_global.display,0,small_font,
-				10,10,ALIGN_LEFT,ALIGN_TOP,cbuf);
+		for (i = 0; i < 32; i++)
+			s_dt += (frame_times[i] - av_dt) * (frame_times[i] - av_dt);
+		s_dt = sqrt(s_dt / 32);
+		sprintf(cbuf, "average dt = %3.2f (stddev = %3.2f)", av_dt, s_dt);
+		text_render(sprite_global.display, 0, small_font, 10, 10, ALIGN_LEFT, ALIGN_TOP, cbuf);
 	}
 }
 
@@ -305,17 +280,17 @@ void console_frame(void)
 }
 
 /* choices is null terminated. Return the
-   index of the choosen item, or -1 if escape was
-   pressed.*/
+ index of the choosen item, or -1 if escape was
+ pressed.*/
 int select_mode(char *header, char *choices[])
 {
-	int i,key,choice = 0;
+	int i, key, choice = 0;
 	while (1)
 	{
 		console_clear();
 		console_write(header);
 		console_write("\n");
-		for (i=0;choices[i];i++)
+		for (i = 0; choices[i]; i++)
 		{
 			if (choice == i)
 				console_write("*");
@@ -355,9 +330,9 @@ void player_keys_mode(int p)
 {
 	int i;
 	int key;
-	char cbuf[2] = {0,0};
-	again:
-	console_clear();
+	char cbuf[2] =
+	{ 0, 0 };
+	again: console_clear();
 	console_write("Setting up key bindings,\nhit the appropriate keys.\n");
 	i = 0;
 	while (control_key_names[i].signal)
@@ -381,8 +356,8 @@ void player_keys_mode(int p)
 
 void player_setup_mode(void)
 {
-	char *items[] = {0,0,"Left player keys",
-			"Right player keys","Go back",0};
+	char *items[] =
+	{ 0, 0, "Left player keys", "Right player keys", "Go back", 0 };
 	while (1)
 	{
 		if (players[1].ishuman)
@@ -394,7 +369,7 @@ void player_setup_mode(void)
 		else
 			items[1] = "Right Player: AI";
 
-		switch (select_mode("Player setup",items))
+		switch (select_mode("Player setup", items))
 		{
 		case 0:
 			players[1].ishuman = 1 - players[1].ishuman;
@@ -431,9 +406,7 @@ void message_mode(char *message)
 	sprite_group_draw(effects_group);
 	sprite_group_draw(foreground_group);
 	sprite_group_draw(ui_group);
-	text_render(sprite_global.display, 0, big_font,
-			sprite_global.display->w/2, sprite_global.display->h/2,
-			ALIGN_CENTER, ALIGN_CENTER, message);
+	text_render(sprite_global.display, 0, big_font, sprite_global.display->w / 2, sprite_global.display->h / 2, ALIGN_CENTER, ALIGN_CENTER, message);
 	sprite_end_frame();
 	sprite_dirty_all();
 	wait_for_key();
@@ -441,19 +414,19 @@ void message_mode(char *message)
 
 void about_mode(char *filename)
 {
-	FILE *message = fopen(path_to_data(filename),"r");
+	FILE *message = fopen(path_to_data(filename), "r");
 	char cbuf[2001];
 	size_t n;
 
 	if (message)
 	{
-		n = fread(cbuf,1,2000,message);
+		n = fread(cbuf, 1, 2000, message);
 		cbuf[n] = 0;
 		fclose(message);
 	}
 	else
 	{
-		sprintf(cbuf,"Error: could not open '%s'.",filename);
+		sprintf(cbuf, "Error: could not open '%s'.", filename);
 	}
 	message_mode(cbuf);
 }
@@ -461,19 +434,10 @@ void about_mode(char *filename)
 void console_mode()
 {
 	char *items[] =
-	{
-			"Resume game",
-			"Players setup",
-			"Toggle fullscreen",
-			"About the game",
-			"Release notes",
-			"Toggle debug mode",
-			"Quit game",
-			0
-	};
+	{ "Resume game", "Players setup", "Toggle fullscreen", "About the game", "Release notes", "Toggle debug mode", "Quit game", 0 };
 	while (1)
 	{
-		switch (select_mode("Airstrike menu",items))
+		switch (select_mode("Airstrike menu", items))
 		{
 		case 0:
 			return;
@@ -483,8 +447,7 @@ void console_mode()
 			break;
 		case 2:
 			displayflags ^= SDL_FULLSCREEN;
-			sprite_global.display =
-					SDL_SetVideoMode(screen_w, screen_h, 0, displayflags);
+			sprite_global.display = SDL_SetVideoMode(screen_w, screen_h, 0, displayflags);
 			assert(sprite_global.display);
 			sprite_dirty_all();
 			sprite_bg_dirty_all();
@@ -516,7 +479,7 @@ void scorekeeper()
 	int i;
 
 	/* This loop relies on MAXPLAYERS = 2 (great programming..)*/
-	for (i=0;i<playerCount;i++)
+	for (i = 0; i < playerCount; i++)
 	{
 		if (!sprite_isvalid(&(players[i].controller->target)))
 		{
@@ -524,14 +487,15 @@ void scorekeeper()
 			if (players[i].points <= 0)
 			{
 				/*if (i == 0)
-					sprintf(cbuf,"And the winner is %s, by %i point(s)",
-							player_name[1],player_points[1]);
-				else
-					sprintf(cbuf,"And the winner is %s, by %i point(s)",
-							player_name[0],player_points[0]);**/
+				 sprintf(cbuf,"And the winner is %s, by %i point(s)",
+				 player_name[1],player_points[1]);
+				 else
+				 sprintf(cbuf,"And the winner is %s, by %i point(s)",
+				 player_name[0],player_points[0]);**/
 				message_mode(cbuf);
 				int j;
-				for (j=0;j<playerCount;j++){
+				for (j = 0; j < playerCount; j++)
+				{
 					sprite_kill(players[j].sprite);
 					players[j].points = max_points;
 				}
@@ -541,24 +505,22 @@ void scorekeeper()
 			{
 				sprite_release(&(players[i].sprite));
 				s = sprite_create(players[i].sprite_type);
-				sprite_aquire(s,&(players[i].sprite));
+				sprite_aquire(s, &(players[i].sprite));
 				sprite_aquire(players[i].sprite, &(players[i].controller->target));
 				assert(sprite_isvalid(&(players[i].controller->target)));
 				s = players[i].sprite;
-				sprite_group_insert(mech_group,s);
-				sprite_set_pos(s,players[i].startpos[0],players[i].startpos[1]);
+				sprite_group_insert(mech_group, s);
+				sprite_set_pos(s, players[i].startpos[0], players[i].startpos[1]);
 				if (!players[i].ishuman)
-					ai_controller_set_enemy(players[i].controller,players[(i+1)%playerCount].sprite);
+					ai_controller_set_enemy(players[i].controller, players[(i + 1) % playerCount].sprite);
 				/*if (!player_ishuman[1-i])
-				{
-					ai_controller_set_enemy(player_controller[1-i],player_sprite[i]);
-				}*/
+				 {
+				 ai_controller_set_enemy(player_controller[1-i],player_sprite[i]);
+				 }*/
 				s = sprite_create(&energymeter);
-				sprite_set_pos(s,
-						(sprite_global.display->w - 60)*(1 - i) + 30,
-						sprite_global.display->h - 30);
-				sprite_signal(s,SIGNAL_SPRITETARGET,players[i].sprite);
-				sprite_group_insert(ui_group,s);
+				sprite_set_pos(s, (sprite_global.display->w - 60) * (1 - i) + 30, sprite_global.display->h - 30);
+				sprite_signal(s, SIGNAL_SPRITETARGET, players[i].sprite);
+				sprite_group_insert(ui_group, s);
 			}
 		}
 	}
@@ -591,18 +553,14 @@ void game_frame()
 
 	mech_gravity(mech_group); /* has to be before bg_coll */
 
-	sprite_group_coll(mech_group,mech_sprite_collide);
-	sprite_group_coll2(bullet_group,mech_group,0);
-	sprite_group_bg_coll(mech_group,mech_sprite_bg_collide);
-	sprite_group_bg_coll(bullet_group,0);
+	sprite_group_coll(mech_group, mech_sprite_collide);
+	sprite_group_coll2(bullet_group, mech_group, 0);
+	sprite_group_bg_coll(mech_group, mech_sprite_bg_collide);
+	sprite_group_bg_coll(bullet_group, 0);
 
-
-
-	for (i=0;i<playerCount;i++)
+	for (i = 0; i < playerCount; i++)
 	{
-		printf("Update...\n");
 		players[i].controller->update(players[i].controller);
-		printf("Updated\n");
 	}
 
 	sprite_group_update(mech_group);
@@ -618,7 +576,7 @@ void game_frame()
 	sprite_group_cleanup(ui_group);
 
 	/* Refresh sprite positions to reflect changes
-     by collisions */
+	 by collisions */
 
 	sprite_group_pos_update(mech_group);
 
@@ -652,12 +610,11 @@ void saveanimframe()
 	r.h = 96;
 	if (!img)
 	{
-		img = SDL_CreateRGBSurface(SDL_SWSURFACE,
-				r.w,r.h,8,0,0,0,0);
+		img = SDL_CreateRGBSurface(SDL_SWSURFACE, r.w, r.h, 8, 0, 0, 0, 0);
 		img = SDL_DisplayFormat(img);
 	}
-	SDL_BlitSurface(sprite_global.display,&r,img,0);
-	sprintf(filename,"%05i.bmp",n++);
+	SDL_BlitSurface(sprite_global.display, &r, img, 0);
+	sprintf(filename, "%05i.bmp", n++);
 	SDL_SaveBMP(img,filename);
 }
 
@@ -665,14 +622,17 @@ int main(int argc, char *argv[])
 {
 	int res;
 	if (argc < 3 || argc > 3)
-		{
-			printf("Airstrike nbOfTeams nbOfPlayers\n");
-			exit(EXIT_SUCCESS);
-		}else if (argc = 2){
-			printf("%d teams\n%d players\n",(int)argv[1],(int)argv[2]);
-			nbTeams = (int)argv[1];
-			nbPlayers = (int)argv[2];
-		}
+	{
+		printf("Airstrike nbOfTeams nbOfPlayers\n");
+		exit(EXIT_SUCCESS);
+	}
+	else if (argc = 2)
+	{
+		nbTeams = (int) strtol(argv[1], &argv[1], 10);
+		nbPlayers = (int) strtol(argv[2], &argv[2], 10);
+		printf("%d teams\n%d players\n", nbTeams, nbPlayers);
+
+	}
 
 	//prototype_setup();
 
@@ -684,12 +644,11 @@ int main(int argc, char *argv[])
 	DEBUGPOINT(1);
 	players_setup();
 	network_init();
-	fprintf(stderr,"Entering main loop.\n");
-	while(process_events())
+	fprintf(stderr, "Entering main loop.\n");
+	while (process_events())
 	{
 		if (!paused)
 		{
-			printf("Main loop !\n");
 			game_frame();
 		}
 		else
@@ -699,7 +658,7 @@ int main(int argc, char *argv[])
 		}
 	}
 	/* We probably never get here right now, because of exit() calls. */
-	fprintf(stderr,"Left main loop.\n");
+	fprintf(stderr, "Left main loop.\n");
 	sprite_group_free(mech_group);
 	sprite_group_free(bullet_group);
 	sprite_group_free(effects_group);
