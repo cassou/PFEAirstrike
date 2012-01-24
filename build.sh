96b3d48c17a08ae034866a8ba3aacc6309b3166a
sauvage <noki.sauvage@gmail.com>
2012-01-24 12:26:25 +0100

    Clients et script de build

--- build.sh
+++ build.sh
@@ -49,12 +49,13 @@ create_package_qt () {
     mkdir -p Release/ClientLinux/lib
     mkdir -p Release/ClientLinux/Solo
     cp QtClient/QtClient Release/ClientLinux/bin/
-    cp enet/enet-1.3.3/.libs/libenet.so Release/ClientLinux/lib/
+    cp enet/enet-1.3.3/.libs/libenet.so Release/ClientLinux/lib/libenet.so.1
     cp Sprites/Solo/* Release/ClientLinux/Solo/
     cp QtClient/start.sh Release/ClientLinux/
     cp QtClient/lib/libQtCore.so.4.7.4 Release/ClientLinux/lib/libQtCore.so
     cp QtClient/lib/libQtGui.so.4.7.4 Release/ClientLinux/lib/libQtGui.so
-    tar -czvf clientLinux.tar.gz Release/ClientLinux
+    cp QtClient/README Release/ClientLinux/README
+    tar -czvf Release/clientLinux.tar.gz Release/ClientLinux
     echo "Done"
 }
 
